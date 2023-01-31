87554a39e925640fc24db38f927f255b03a66b59
Jul <julklingel@Juls-MacBook-Pro.local>
2023-01-31 15:31:03 -0500

    added component folder

--- frontend/turbomarket/src/pages/index.tsx
+++ frontend/turbomarket/src/pages/index.tsx
@@ -1,10 +1,10 @@
-
+import Navbar from "./components/navbar"
 
 export default function Home() {
   return (
 
     <div>
-
+      <Navbar />
       <h1 className="text-3xl bold underline-offset-2 bg-orange-600">Hello World!</h1>
 
 
