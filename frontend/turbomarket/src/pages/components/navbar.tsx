d9169e05ba0e205f2c615c98b42f7072c418bc88
Jul <julklingel@Juls-MacBook-Pro.local>
2023-01-31 15:34:31 -0500

    added ul list

--- frontend/turbomarket/src/pages/components/navbar.tsx
+++ frontend/turbomarket/src/pages/components/navbar.tsx
@@ -3,6 +3,11 @@
     return (
         <div>
             <h1>Navbar</h1>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>Cart</li>
+            </ul>
         </div>
     )
 }
