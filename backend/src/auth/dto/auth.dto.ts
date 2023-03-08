6b4d2031c75b0ca490a9d8f778397ef9a2089a84
juro95 <79195394+juro95@users.noreply.github.com>
2023-03-08 16:20:37 +0100

    Basic swagger setup and schema for auth

--- backend/src/auth/dto/auth.dto.ts
+++ backend/src/auth/dto/auth.dto.ts
@@ -1,19 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class signupDto {
+  @ApiProperty({ example: 'john@example.com' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({ example: 'pass123' })
   @IsString()
   @IsNotEmpty()
   password: string;
 
+  @ApiProperty({ example: 'john@example.com' })
   //@IsBoolean()
   isSupplier: boolean;
 }
 
 export class loginDto {
+  @ApiProperty({ example: 'pass123' })
   @IsEmail()
   @IsNotEmpty()
   email: string;
