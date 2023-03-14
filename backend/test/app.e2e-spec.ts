3b87abfe6caec5b9296f4c542c797953918d810f
juro95 <79195394+juro95@users.noreply.github.com>
2023-03-14 11:36:40 +0100

    setup test db

--- backend/test/app.e2e-spec.ts
+++ backend/test/app.e2e-spec.ts
@@ -1,11 +1,22 @@
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app/app.module';
 
 describe('App e2e test', () => {
+  let app: INestApplication;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({ whitelist: true, transform: true }),
+    );
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
   it.todo('should pass');
 });
