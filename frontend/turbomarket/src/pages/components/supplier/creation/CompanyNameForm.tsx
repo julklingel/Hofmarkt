f03d39797b957059b92851316db772f01a63de87
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-16 00:51:59 +0100

    gute nacht...

--- frontend/turbomarket/src/pages/components/supplier/creation/CompanyNameForm.tsx
+++ frontend/turbomarket/src/pages/components/supplier/creation/CompanyNameForm.tsx
@@ -127,6 +127,7 @@ export default function CompanyNameForm() {
                             className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
                             placeholder="Mobile Number"
                             ref={mobileNumber}
+                            required
                           />
                         </div>
                       </div>
