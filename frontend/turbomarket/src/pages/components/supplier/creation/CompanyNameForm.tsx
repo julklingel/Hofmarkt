2875f76f8c76795b19412f6d3299222cc09fcbc1
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-15 19:03:53 +0100

    anything

--- frontend/turbomarket/src/pages/components/supplier/creation/CompanyNameForm.tsx
+++ frontend/turbomarket/src/pages/components/supplier/creation/CompanyNameForm.tsx
@@ -1,28 +1,73 @@
-import React, { Fragment, useRef, useContext } from "react";
-import { ViewContext } from "../../../../../store/userCreation/NavigationContextUser";
-import { PersonalDataContext, PersonalData, AddressData } from "../../../../../store/userCreation/DataContextUser";
-import {FormEvent} from "react";
+import React, { Fragment, useRef, useState, useContext } from "react";
+import { ViewContext } from "store/supplierCreation/NavigationContextSupplier";
+import { NameFormDataContext } from "store/supplierCreation/DataContextSupplier";
+import { FormEvent } from "react";
+
+
+const formatFiles = async (input: any) => {
+  const formData = new FormData();
+  input.forEach((file: any) => {
+    formData.append('files', file);
+  });
+  return formData;
+}
 
 
 
-type PersonalDataContextType = {
-  personalData: PersonalData;
-  setPersonalData: React.Dispatch<React.SetStateAction<PersonalData>>;
-  addressData: AddressData;
-  setAddressData: React.Dispatch<React.SetStateAction<AddressData>>;
-};
 
 export default function CompanyNameForm() {
+  const [facilityImages, setFacilityImages] = useState([]);
+  const [profileImage, setProfileImage] = useState([]);
+  const { currentView, setCurrentView } = useContext(ViewContext);
+  const [nameFormData, setNameFormData] = useState(NameFormDataContext);
+  const []  = useState();
+
+  const companyWebsite = useRef<HTMLInputElement>(null);
+  const aboutText = useRef<HTMLTextAreaElement>(null);
+  const mobileNumber = useRef<HTMLInputElement>(null);
+
+const handleFacilityImages = (e: any) => {
+  setFacilityImages(Array.from(e.target.files));
+  console.log(facilityImages);
+  
+};
+
+
+  const  handleProfileImage = (e: any) => {
+    setProfileImage(e.target.files);
+  };
+
+
+
+
+  function handleSubmit(e: FormEvent<HTMLButtonElement> ) {
+    e.preventDefault();
+    
+    const newNameData = {
+      website: companyWebsite.current.value,
+      mobileNumber: mobileNumber.current.value,
+      About: aboutText.current.value,
+      facilityImages: facilityImages,
+      profileImage: profileImage,
+    };
+
+    console.log(newNameData);
+    
+
+  }
+
   return (
     <Fragment>
-      {/* PAGE 1 */}
       <div>
         <div className="md:grid md:grid-cols-2 mx-40 md:gap-6  ">
           <div className="md:col-span-1">
             <div className="px-4 sm:px-0">
-              <h3 className="text-base font-semibold leading-6 text-gray-900">Your Profile</h3>
+              <h3 className="text-base font-semibold leading-6 text-gray-900">
+                Your Profile
+              </h3>
               <p className="mt-1 text-sm text-gray-600">
-                This information will be displayed publicly so be careful what you share.
+                This information will be displayed publicly so be careful what
+                you share.
               </p>
             </div>
           </div>
@@ -31,48 +76,59 @@ export default function CompanyNameForm() {
               <div className="shadow sm:overflow-hidden rounded-3xl">
                 <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
                   <div className="grid grid-cols-2 gap-6">
-                  <div className="grid">
-                    <div className="">
-                      <label htmlFor="company-website" className="block text-sm font-medium leading-6 text-gray-900">
-                        Website
-                      </label>
-                      <div className="mt-2 flex rounded-md shadow-sm">
-                        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">
-                          http://
-                        </span>
-                        <input
-                          type="text"
-                          name="company-website"
-                          id="company-website"
-                          className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
-                          placeholder="www.example.com"
-                        />
+                    <div className="grid">
+                      <div className="">
+                        <label
+                          htmlFor="company-website"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Website
+                        </label>
+                        <div className="mt-2 flex rounded-md shadow-sm">
+                          <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">
+                            http://
+                          </span>
+                          <input
+                            type="text"
+                            name="company-website"
+                            id="company-website"
+                            className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                            placeholder="www.example.com"
+                            ref={companyWebsite}
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div className="grid">
-                    <div className="col-span-3 sm:col-span-1">
-                      <label htmlFor="company-website" className="block text-sm font-medium leading-6 text-gray-900">
-                        Mobile Number
-                      </label>
-                      <div className="mt-2 flex rounded-md shadow-sm">
-                        <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">
-                          tel:
-                        </span>
-                        <input
-                          type="text"
-                          name="company-number"
-                          id="company-number"
-                          className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
-                          placeholder="Mobile Number"
-                        />
+                    <div className="grid">
+                      <div className="col-span-3 sm:col-span-1">
+                        <label
+                          htmlFor="company-website"
+                          className="block text-sm font-medium leading-6 text-gray-900"
+                        >
+                          Mobile Number
+                        </label>
+                        <div className="mt-2 flex rounded-md shadow-sm">
+                          <span className="inline-flex items-center rounded-l-md border border-r-0 border-gray-300 px-3 text-gray-500 sm:text-sm">
+                            tel:
+                          </span>
+                          <input
+                            type="text"
+                            name="company-number"
+                            id="company-number"
+                            className="block w-full flex-1 rounded-none rounded-r-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:text-sm sm:leading-6"
+                            placeholder="Mobile Number"
+                            ref={mobileNumber}
+                          />
+                        </div>
                       </div>
                     </div>
-                  </div>
                   </div>
 
                   <div>
-                    <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                    <label
+                      htmlFor="about"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
                       About
                     </label>
                     <div className="mt-2">
@@ -82,33 +138,52 @@ export default function CompanyNameForm() {
                         rows={3}
                         className="mt-1 block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-green-600 sm:py-1.5 sm:text-sm sm:leading-6"
                         placeholder="Thats how I bake my cakes..."
-                        defaultValue={''}
+                        defaultValue={""}
+                        ref={aboutText}
                       />
                     </div>
                     <p className="mt-2 text-sm text-gray-500">
-                      Brief description for your profile. Tell everyone how awesome you are.
+                      Brief description for your profile. Tell everyone how
+                      awesome you are.
                     </p>
                   </div>
 
                   <div>
-                    <label className="block text-sm font-medium leading-6 text-gray-900">Photo</label>
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      Photo
+                    </label>
                     <div className="mt-2 flex items-center">
                       <span className="inline-block h-12 w-12 overflow-hidden rounded-full bg-gray-100">
-                        <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                        <svg
+                          className="h-full w-full text-gray-300"
+                          fill="currentColor"
+                          viewBox="0 0 24 24"
+                        >
                           <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
                         </svg>
                       </span>
-                      <button
-                        type="button"
-                        className="ml-5 rounded-md border border-gray-300 bg-white py-1.5 px-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-50"
-                      >
-                        Change
-                      </button>
+                   
+                         <label
+                            htmlFor="file-upload"
+                            className="ml-5 rounded-md border border-gray-300 bg-white py-1.5 px-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-200"
+                          >
+                            <span>Change</span>
+                            <input
+                              id="file-upload"
+                              name="file-upload"
+                              type="file"
+                              className="sr-only"
+                              onChange={handleProfileImage}
+                            />
+                          </label>
+               
                     </div>
                   </div>
 
                   <div>
-                    <label className="block text-sm font-medium leading-6 text-gray-900">Photos of your Facility</label>
+                    <label className="block text-sm font-medium leading-6 text-gray-900">
+                      Photos of your Facility
+                    </label>
                     <div className="mt-2 flex justify-center rounded-md border-2 border-dashed border-gray-300 px-6 pt-5 pb-6">
                       <div className="space-y-1 text-center">
                         <svg
@@ -131,11 +206,20 @@ export default function CompanyNameForm() {
                             className="relative cursor-pointer rounded-md bg-white font-medium text-green-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-500 focus-within:ring-offset-2 hover:text-green-500"
                           >
                             <span>Upload a file</span>
-                            <input id="file-upload" name="file-upload" type="file" className="sr-only" multiple />
+                            <input
+                              id="file-upload"
+                              name="file-upload"
+                              type="file"
+                              className="sr-only"
+                              multiple
+                              onChange={handleFacilityImages}
+                            />
                           </label>
                           <p className="pl-1">or drag and drop</p>
                         </div>
-                        <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+                        <p className="text-xs text-gray-500">
+                          PNG, JPG, GIF up to 10MB
+                        </p>
                       </div>
                     </div>
                   </div>
@@ -143,6 +227,7 @@ export default function CompanyNameForm() {
                 <div className="bg-white px-4 py-3 text-right sm:px-6">
                   <button
                     type="submit"
+                    onClick={handleSubmit}
                     className="inline-flex justify-center rounded-md bg-green-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-500"
                   >
                     Save
@@ -153,13 +238,6 @@ export default function CompanyNameForm() {
           </div>
         </div>
       </div>
-    
-
-      
-
-
-     
     </Fragment>
-  )
+  );
 }
-
