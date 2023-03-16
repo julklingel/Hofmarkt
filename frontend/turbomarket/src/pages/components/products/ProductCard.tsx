77f21f55184082d4409f8b18ab2a1e0a7bc5cc29
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-16 16:29:23 +0100

    added amount

--- frontend/turbomarket/src/pages/components/products/ProductCard.tsx
+++ frontend/turbomarket/src/pages/components/products/ProductCard.tsx
@@ -14,6 +14,7 @@ export default function ProductCard(props: any) {
   const linkPath = `/offer/${slug}/${id}`;
   const imgPath = `/images/supplier/${slug}/offers/${img}`;
 
+
   return (
     <Link href={linkPath}>
       <div className="bg-secondary rounded-2xl shadow-2xl m-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
@@ -37,6 +38,8 @@ export default function ProductCard(props: any) {
         <h3 className="mt-2 text-xl text-c.green">{title}</h3>
         <div className="flex items-center justify-between">
           <span className="text-l mx-3 text-gray-900 ">${price}</span>
+          <span className=" text-xs mx-1 text-gray-900 ">3stk.</span>
+
           <button className=" mx-2 p-1 px-1.5 m-1 text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm  text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
             Add to cart
           </button>
