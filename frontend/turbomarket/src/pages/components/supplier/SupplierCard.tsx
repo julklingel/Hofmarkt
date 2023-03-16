cc24f2d32ef94b66413cfd71a416fc252874e25c
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-16 15:32:52 +0100

    fancy pancy loading effect

--- frontend/turbomarket/src/pages/components/supplier/SupplierCard.tsx
+++ frontend/turbomarket/src/pages/components/supplier/SupplierCard.tsx
@@ -1,26 +1,44 @@
-import Link from "next/link"
-import Image from "next/image"
-import { title } from "process";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
 
-
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 export default function SupplierCard(props: any) {
+  const [isLoading, setLoading] = useState(true);
 
-    const { companyName, companyImage, companyAddress, slug } = props.supplier;
-    const linkPath = `/supplier/${slug}`
-    const imgPath = `/images/supplier/${slug}/${companyImage}`
+  const { companyName, companyImage, companyAddress, slug } = props.supplier;
+  const linkPath = `/supplier/${slug}`;
+  const imgPath = `/images/supplier/${slug}/${companyImage}`;
 
-    return (
-        <li className=' bg-secondary rounded-3xl transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300'>
-            <Link href={linkPath}>
-                <div>
-                    <Image src={imgPath} alt={companyName} width={300} height={200} layout='responsive' />
-                </div>
-                <div>
-                    <h3>{companyName}</h3>
-                    <p>{companyAddress}</p>
-                </div>
-            </Link>
-        </li>
-    )
+  return (
+    <li >
+      <Link href={linkPath}>
+        <div className=" bg-secondary rounded-2xl shadow-2xl">
+        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+          <Image
+            alt=""
+            src={imgPath}
+            width={500}
+            height={500}
+            layout="responsive"
+            className={cn(
+              "duration-700 ease-in-out group-hover:opacity-75",
+              isLoading
+                ? "scale-110 blur-2xl grayscale"
+                : "scale-100 blur-0 grayscale-0"
+            )}
+            onLoadingComplete={() => setLoading(false)}
+          />
+        </div>
+        <h3 className="mt-4 text-lg text-c.green">{companyName}</h3>
+        <p className="mt-1 text-sm font-medium text-gray-700">
+          {companyAddress}
+        </p>
+        </div>
+      </Link>
+    </li>
+  );
 }
--- frontend/turbomarket/src/pages/components/supplier/SupplierGrid.tsx
+++ frontend/turbomarket/src/pages/components/supplier/SupplierGrid.tsx
@@ -1,21 +1,25 @@
 import SupplierCard from "./SupplierCard";
 import React from "react";
 
+export default function SupplierGrid(props: any) {
+  const suppliers = props.suppliers;
 
-
-
-export default function SupplierGrid(props:any) {
-    const suppliers = props.suppliers;
-    
-    if (!suppliers) {
-        return <div className=" h-screen text-center text-xl p-5 text-c.green ">No supplier found</div>
-}
+  if (!suppliers) {
     return (
-        <div className="">
-            <ul className="grid grid-cols-4 content-center gap-6">
-                {suppliers.supplierObj.map((supplier:any) => (<SupplierCard key={supplier.slug} supplier={supplier}/>))}           
-            </ul> 
-            
-        </div>
-    )
+      <div className=" h-screen text-center text-xl p-5 text-c.green ">
+        No supplier found
+      </div>
+    );
+  }
+  return (
+    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
+      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <ul>
+          {suppliers.supplierObj.map((supplier: any) => (
+            <SupplierCard key={supplier.slug} supplier={supplier} />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
