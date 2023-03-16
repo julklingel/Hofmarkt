cc0276131119fd95cb70e423a72c7cbc1e99609d
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-16 16:07:07 +0100

    limited height on cards and add reviews

--- frontend/turbomarket/src/pages/components/supplier/SupplierCard.tsx
+++ frontend/turbomarket/src/pages/components/supplier/SupplierCard.tsx
@@ -14,10 +14,9 @@ export default function SupplierCard(props: any) {
   const imgPath = `/images/supplier/${slug}/${companyImage}`;
 
   return (
-    <li >
-      <Link href={linkPath}>
-        <div className=" bg-secondary rounded-2xl shadow-2xl">
-        <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
+    <Link href={linkPath}>
+      <div className="bg-secondary rounded-2xl shadow-2xl m-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 duration-300">
+        <div className="w-full h-48 overflow-hidden rounded-lg bg-secondary relative">
           <Image
             alt=""
             src={imgPath}
@@ -25,7 +24,7 @@ export default function SupplierCard(props: any) {
             height={500}
             layout="responsive"
             className={cn(
-              "duration-700 ease-in-out group-hover:opacity-75",
+              "duration-700 ease-in-out group-hover:opacity-75 object-cover object-center absolute inset-0",
               isLoading
                 ? "scale-110 blur-2xl grayscale"
                 : "scale-100 blur-0 grayscale-0"
@@ -33,12 +32,66 @@ export default function SupplierCard(props: any) {
             onLoadingComplete={() => setLoading(false)}
           />
         </div>
-        <h3 className="mt-4 text-lg text-c.green">{companyName}</h3>
+        
+        <h3 className="mt-2 text-lg text-c.green">{companyName}</h3>
         <p className="mt-1 text-sm font-medium text-gray-700">
           {companyAddress}
         </p>
+
+        <div className="flex justify-center">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-green-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>First star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-green-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Second star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-green-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Third star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-green-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Fourth star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-green-400"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Fifth star</title>
+            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+          </svg>
+        
         </div>
-      </Link>
-    </li>
+      </div>
+    </Link>
   );
 }
--- frontend/turbomarket/src/pages/components/supplier/SupplierGrid.tsx
+++ frontend/turbomarket/src/pages/components/supplier/SupplierGrid.tsx
@@ -14,11 +14,10 @@ export default function SupplierGrid(props: any) {
   return (
     <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <ul>
+        
           {suppliers.supplierObj.map((supplier: any) => (
             <SupplierCard key={supplier.slug} supplier={supplier} />
           ))}
-        </ul>
       </div>
     </div>
   );
