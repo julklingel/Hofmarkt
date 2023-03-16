7bddc8b601eff3c9e5516da117ca710aeaf39fd6
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-16 15:36:10 +0100

    format stuff

--- frontend/turbomarket/src/pages/supplier/AllSuppliers.tsx
+++ frontend/turbomarket/src/pages/supplier/AllSuppliers.tsx
@@ -8,7 +8,7 @@ export default function AllSupplier(props: any) {
     
     return (
         
-        <section className=" px-10 text-center text-c.green ">
+        <section className="text-center text-c.green ">
         <SupplierGrid suppliers={props.suppliers}/>
         </section>
         
--- frontend/turbomarket/src/pages/supplier/index.tsx
+++ frontend/turbomarket/src/pages/supplier/index.tsx
@@ -1,35 +1,25 @@
-
-
-
 import { Fragment } from "react";
 import Navbar from "../components/navbar";
 import AllSuppliers from "./AllSuppliers";
 
-
-
-
 export default function AllSuppliersPage(supplierObj: any) {
-    const supplier = supplierObj
+  const supplier = supplierObj;
 
-    
-    return (
-            
-        <Fragment>
-            <Navbar />
-            <AllSuppliers suppliers={supplier}/>
-        </Fragment>
-        
-    );
-    }
+  return (
+    <Fragment>
+      <Navbar />
+      <AllSuppliers suppliers={supplier} />
+    </Fragment>
+  );
+}
 
 export async function getStaticProps() {
-    const res = await fetch('http://localhost:4444/supplier');
-    const supplierObj = await res.json();
-    
-    
-    return {
-        props: {
-            supplierObj
-        }
-    }
+  const res = await fetch("http://localhost:4444/supplier");
+  const supplierObj = await res.json();
+
+  return {
+    props: {
+      supplierObj,
+    },
+  };
 }
