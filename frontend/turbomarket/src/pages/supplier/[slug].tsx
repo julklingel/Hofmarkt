af80a0688d8b2491819d5b5e017f5a289d58f560
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-20 16:26:19 +0100

    structure

--- frontend/turbomarket/src/pages/supplier/[slug].tsx
+++ frontend/turbomarket/src/pages/supplier/[slug].tsx
@@ -1,3 +1,6 @@
+import { Fragment } from "react";
+import Navbar from "../components/navbar";
+import SupplierContent from "./supplier-detail/SupplierContent";
 
 
 
@@ -6,12 +9,9 @@
 
 export default function Supplier() {
     return (
-        <div>
-            <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-20 px-14 text-center">
-                <title>Supplier</title>
-                <h1>Supplie Single</h1>
-                <link rel="icon" href="/favicon.ico" />
-            </div>
-        </div>
+       <Fragment>
+        <Navbar/>
+        <SupplierContent/>
+       </Fragment>
     )
 }
--- frontend/turbomarket/src/pages/supplier/supplier-detail/SupplierContent.tsx
+++ frontend/turbomarket/src/pages/supplier/supplier-detail/SupplierContent.tsx
@@ -1,39 +1,70 @@
-import SupplierHeader from "./SupplierHeader";
+import { Fragment } from "react";
+import Image from "next/image";
 
-const DUMMY_SUPPLIERS =
-{
-    id: 's1',
-    name: 'Imkerei Ammersee GmbH',
-    category: 'Bienenprodukte',
-    image: 'imkerei.jpeg',
-    slug: 'imkerei-ammersee-gmbh',
-    location: 'Ammerseestraße 1, 82515 Wolfratshausen',
-    date: '2021-01-01',
-    products: [
-        {
-            id: 'p1',
-            name: 'Wald-Honig',
-            image: 'honig.jpg',
-            price: 5.99,
-            description: 'Honig aus der Region',
-        },
-        {
-            id: 'p2',
-            name: 'Bienenwachs',
-            image: 'bienenwachs.jpg',
-            price: 9.99,
-            description: 'Bienenwachs aus der Region',
-        },
-    ]
-}
-
+const DUMMY_SUPPLIERS = {
+  id: "s1",
+  name: "Imkerei Ammersee GmbH",
+  catSvgLink: "/svgs/honey.svg",
+  companyImage: "Imkerei.jpeg",
+  slug: "ammer-imker",
+  bio: "Die Imkerei Ammersee GmbH ist ein Familienunternehmen, das sich auf die Herstellung von Bienenprodukten spezialisiert hat. Die Produkte werden in der Region produziert und sind von höchster Qualität.",
+  zip: "82319",
+  city: "Starnberg",
+  products: [
+    {
+      id: "p1",
+      name: "Wald-Honig",
+      image: "honig.jpg",
+      price: 5.99,
+      description: "Honig aus der Region",
+    },
+    {
+      id: "p2",
+      name: "Bienenwachs",
+      image: "bienenwachs.jpg",
+      price: 9.99,
+      description: "Bienenwachs aus der Region",
+    },
+  ],
+};
 
 export default function SupplierContent(props: any) {
-    
-    return (
-        <article>
-            <SupplierHeader name='' image='' />
-            <h1>CONTENT</h1>
-        </article>
-    )
+  const { slug, companyImage } = DUMMY_SUPPLIERS;
+  const imgPath = `/images/supplier/${slug}/${companyImage}`;
+  return (
+    <Fragment>
+      <div className="grid grid-cols-2 gap-16 mx-32 mt-16">
+        <section className="text-c.green">
+          <div className="grid grid-cols-2">
+            <h1 className="text-3xl font-bold">{DUMMY_SUPPLIERS.name}</h1>
+            <div className="flex justify-end">
+              <Image
+                src={DUMMY_SUPPLIERS.catSvgLink}
+                width={50}
+                height={50}
+                alt={DUMMY_SUPPLIERS.slug}
+              />
+            </div>
+            <p>
+              {DUMMY_SUPPLIERS.zip}, {DUMMY_SUPPLIERS.city}
+            </p>
+          </div>
+          <p className=" my-6 text-justify font-semibold">
+            {DUMMY_SUPPLIERS.bio}
+          </p>
+        </section>
+        <div className=" place-self-center">
+          {" "}
+          <Image
+            src={imgPath}
+            width={500}
+            height={500}
+            alt={imgPath}
+            layout="responsive"
+            className=" rounded-3xl shadow-2xl"
+          />
+        </div>
+      </div>
+    </Fragment>
+  );
 }
