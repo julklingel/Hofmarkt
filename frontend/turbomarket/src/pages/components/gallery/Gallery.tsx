37a8af1de4897fd38ea49f67dae18f4beabb0978
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-21 14:24:23 +0100

    logic for view  toggler

---
+++ frontend/turbomarket/src/pages/components/gallery/Gallery.tsx
@@ -0,0 +1,11 @@
+
+
+
+export default function Gallery() {
+
+    return (
+        <div>
+            <h1>Gallery</h1>
+        </div>
+    )
+}
--- frontend/turbomarket/src/pages/supplier/supplier-detail/SupplierContent.tsx
+++ frontend/turbomarket/src/pages/supplier/supplier-detail/SupplierContent.tsx
@@ -1,9 +1,17 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import Image from "next/image";
 import AllProducts from "../../market/AllProducts";
+import Reviews from "@/pages/components/reviews/Reviews";
+import Gallery from "@/pages/components/gallery/Gallery";
 
 export default function SupplierContent(props: any) {
   const product = props.products;
+  const [selectedView, setSelectedView] = useState("Products");
+
+  function viewToggle(event: React.ChangeEvent<HTMLInputElement>) {
+    setSelectedView(event.target.value);
+  }
+  
 
   return (
     <Fragment>
@@ -11,14 +19,15 @@ export default function SupplierContent(props: any) {
         <ul className="items-center w-full text-sm font-medium  text-c.green rounded-lg shadow-2xl sm:flex dark:bg-secondary">
           <li className="w-full grid grid-cols-6">
             <div className="flex items-center pl-3">
-              <input
-                id="horizontal-list-radio-license"
-                type="radio"
-                value=""
-                name="list-radio"
-                defaultChecked={true}
-                className="w-4 h-4 text-green-600 bg-gray-100  focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
+            <input
+              id="horizontal-list-radio-license"
+              type="radio"
+              value="Products"
+              name="list-radio"
+              defaultChecked={true}
+              className="w-4 h-4 text-green-600 bg-gray-100  focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              onChange={viewToggle}
+            />
               <label
                 htmlFor="horizontal-list-radio-license"
                 className="w-full py-3 ml-2 text-sm font-medium text-gray-900"
@@ -29,13 +38,14 @@ export default function SupplierContent(props: any) {
           </li>
           <li className="w-full ">
             <div className="flex items-center pl-3">
-              <input
-                id="horizontal-list-radio-id"
-                type="radio"
-                value=""
-                name="list-radio"
-                className="w-4 h-4 text-green-600 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              />
+            <input
+              id="horizontal-list-radio-id"
+              type="radio"
+              value="Gallery"
+              name="list-radio"
+              className="w-4 h-4 text-green-600 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+              onChange={viewToggle}
+            />
               <label
                 htmlFor="horizontal-list-radio-id"
                 className="w-full py-3 ml-2 text-sm font-medium text-gray-900"
@@ -47,13 +57,14 @@ export default function SupplierContent(props: any) {
 
           <li className="w-full ">
             <div className="flex items-center pl-3">
-              <input
-                id="horizontal-list-radio-passport"
-                type="radio"
-                value=""
-                name="list-radio"
-                className="w-4 h-4 text-green-600 bg-gray-100 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600"
-              />
+            <input
+              id="horizontal-list-radio-passport"
+              type="radio"
+              value="Reviews"
+              name="list-radio"
+              className="w-4 h-4 text-green-600 bg-gray-100 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600"
+              onChange={viewToggle}
+            />
               <label
                 htmlFor="horizontal-list-radio-passport"
                 className="w-full py-3 ml-2 text-sm font-medium text-green-900 "
@@ -64,7 +75,11 @@ export default function SupplierContent(props: any) {
           </li>
         </ul>
       </section>
-      <AllProducts products={product} />
+      <section>
+        {selectedView === "Products" && <AllProducts products={product} />}
+        {selectedView === "Gallery" && <Gallery />}
+        {selectedView === "Reviews" && <Reviews />}
+      </section>
     </Fragment>
   );
 }
