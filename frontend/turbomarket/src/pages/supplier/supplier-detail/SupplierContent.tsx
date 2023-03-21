1252db3060525356527ed3053368dddfdfeb8731
Jul <julklingel@Juls-MacBook-Pro.local>
2023-03-21 14:25:01 +0100

    format stuff

--- frontend/turbomarket/src/pages/supplier/supplier-detail/SupplierContent.tsx
+++ frontend/turbomarket/src/pages/supplier/supplier-detail/SupplierContent.tsx
@@ -11,7 +11,6 @@ export default function SupplierContent(props: any) {
   function viewToggle(event: React.ChangeEvent<HTMLInputElement>) {
     setSelectedView(event.target.value);
   }
-  
 
   return (
     <Fragment>
@@ -19,15 +18,15 @@ export default function SupplierContent(props: any) {
         <ul className="items-center w-full text-sm font-medium  text-c.green rounded-lg shadow-2xl sm:flex dark:bg-secondary">
           <li className="w-full grid grid-cols-6">
             <div className="flex items-center pl-3">
-            <input
-              id="horizontal-list-radio-license"
-              type="radio"
-              value="Products"
-              name="list-radio"
-              defaultChecked={true}
-              className="w-4 h-4 text-green-600 bg-gray-100  focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              onChange={viewToggle}
-            />
+              <input
+                id="horizontal-list-radio-license"
+                type="radio"
+                value="Products"
+                name="list-radio"
+                defaultChecked={true}
+                className="w-4 h-4 text-green-600 bg-gray-100  focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                onChange={viewToggle}
+              />
               <label
                 htmlFor="horizontal-list-radio-license"
                 className="w-full py-3 ml-2 text-sm font-medium text-gray-900"
@@ -38,14 +37,14 @@ export default function SupplierContent(props: any) {
           </li>
           <li className="w-full ">
             <div className="flex items-center pl-3">
-            <input
-              id="horizontal-list-radio-id"
-              type="radio"
-              value="Gallery"
-              name="list-radio"
-              className="w-4 h-4 text-green-600 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
-              onChange={viewToggle}
-            />
+              <input
+                id="horizontal-list-radio-id"
+                type="radio"
+                value="Gallery"
+                name="list-radio"
+                className="w-4 h-4 text-green-600 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"
+                onChange={viewToggle}
+              />
               <label
                 htmlFor="horizontal-list-radio-id"
                 className="w-full py-3 ml-2 text-sm font-medium text-gray-900"
@@ -57,14 +56,14 @@ export default function SupplierContent(props: any) {
 
           <li className="w-full ">
             <div className="flex items-center pl-3">
-            <input
-              id="horizontal-list-radio-passport"
-              type="radio"
-              value="Reviews"
-              name="list-radio"
-              className="w-4 h-4 text-green-600 bg-gray-100 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600"
-              onChange={viewToggle}
-            />
+              <input
+                id="horizontal-list-radio-passport"
+                type="radio"
+                value="Reviews"
+                name="list-radio"
+                className="w-4 h-4 text-green-600 bg-gray-100 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600"
+                onChange={viewToggle}
+              />
               <label
                 htmlFor="horizontal-list-radio-passport"
                 className="w-full py-3 ml-2 text-sm font-medium text-green-900 "
