105d77080ec51e058a80069a41589814724a77a6
Jul <julklingel@juls-mbp.localdomain>
2023-03-23 22:13:53 +0100

    format hero + new pic

--- frontend/turbomarket/src/pages/components/homepage/hero.tsx
+++ frontend/turbomarket/src/pages/components/homepage/hero.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <section className="grid grid-cols-3 h-5/6 px-10 place-items-stretch">
+    <section className="grid grid-cols-2 h-5/6 px-10 place-items-stretch">
       <div className="place-self-center mb-20 ml-10">
-        <h1 className="text-3xl tracking-widest mb-10 font-semibold text-center text-c.green">
+        <h1 className="text-4xl tracking-widest mb-10 font-semibold ml-16 text-c.green">
           HOF MARKT
         </h1>
-        <p className=" ml-4  text-1xl text-left text-c.green">
+        <p className="text-xl text-left font-semibold text-c.green mx-16">
           Discover the taste of fresh, organic food from local farmers at
           Hofmarkt. Our webshop brings you the best selection of sustainably
           grown produce straight from the fields to your doorstep. Experience
@@ -17,17 +17,17 @@ export default function Hero() {
         </p>
       </div>
 
-      <div className=" col-span-2 place-self-center mb-20 ml-20 ">
+      <div className=" place-self-center mb-20 ml-20 shadow-2xl rounded-full ">
         <Image
-          src="/images/beige-hero.png"
+          src="/svgs/logo.png"
           alt="Hero picture of vegtables on a table"
           className="rounded-3xl"
-          width={500}
-          height={500}
+          width={600}
+          height={600}
         />
       </div>
 
-      <div className="col-span-3 place-self-center ">
+      <div className="col-span-2 place-self-center ">
         <button>
           <Image
             src="/svgs/scroll.svg"
