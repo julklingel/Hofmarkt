ce28d97724eccbcb44141578a5b02ca069486272
Jul <julklingel@Juls-MacBook-Pro.local>
2023-04-22 12:11:32 +0200

    iplement helmet csp

--- backend/src/app/app.service.ts
+++ backend/src/app/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello SAUFI SAUFI! ';
+    return 'Hello from Hofmarkt API!';
   }
 }
